3b4506c6f420082a863987a8fcf31de267a7ad1c
algieili <ilialgie75@gmail.com>
2025-10-27 21:18:09 +0800

    Implement task 3 API endpoints

--- docker-php83/src/routes/api.php
+++ docker-php83/src/routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::apiResource('products', ProductController::class);
+Route::apiResource('tasks', TaskController::class);
